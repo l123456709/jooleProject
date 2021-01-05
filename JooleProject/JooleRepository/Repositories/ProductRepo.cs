f51314d5d2ed14881f83514f5aa95c7d1af45c08
sukeyzhou <luchang1991@gmail.com>
2021-01-05 15:23:49 -0500

    update

--- JooleProject/JooleRepository/Repositories/ProductRepo.cs
+++ JooleProject/JooleRepository/Repositories/ProductRepo.cs
@@ -7,16 +7,15 @@ using System.Web;
 
 namespace JooleRepository.Repositories
 {
-    public interface IProductRepo: IRepository<Product>
+    public interface IProductRepo : IRepository<Product>
     {
 
     }
 
     public class ProductRepo : Repository<Product>, IProductRepo
     {
-        public ProductRepo(DbContext context): base(context)
+        public ProductRepo(DbContext context) : base(context)
         {
 
         }
     }
-}
