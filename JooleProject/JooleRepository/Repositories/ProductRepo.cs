b4cd9809f96f832083c207b55e4d7326a6ee383a
sukeyzhou <luchang1991@gmail.com>
2021-01-05 15:07:59 -0500

    added files

---
+++ JooleProject/JooleRepository/Repositories/ProductRepo.cs
@@ -0,0 +1,22 @@
+﻿using JooleDAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JooleRepository.Repositories
+{
+    public interface IProductRepo: IRepository<Product>
+    {
+
+    }
+
+    public class ProductRepo : Repository<Product>, IProductRepo
+    {
+        public ProductRepo(DbContext context): base(context)
+        {
+
+        }
+    }
+}
---
+++ JooleProject/JooleRepository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using JooleRepository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JooleRepository
+{
+    public class UnitOfWork : IDisposable
+    {
+        DbContext Context;
+        public IProductRepo product;
+
+        public UnitOfWork(DbContext context)
+        {
+            this.Context = context;
+            product = new ProductRepo(Context);
+        }
+
+        public void SaveChanges()
+        {
+            Context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
